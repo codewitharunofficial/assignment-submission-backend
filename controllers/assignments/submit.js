9c98e2206be7ec2c50953af06caf46ee23e15d4c
Arun <arunonwork3249@gmail.com>
2025-06-24 16:24:50 +0530

    Implemented Cloudinary

--- controllers/assignments/submit.js
+++ controllers/assignments/submit.js
@@ -1,60 +1,75 @@
+import cloudinary from "../../DB/cloudinary.js";
 import { dummyUsers } from "../../dummyData.js";
+import assignment from "../../models/assignment.js";
 
 export default async function submit(req, res) {
-    try {
-        const { enrollment_no, program, program_code } = req.fields;
-        const { project_file } = req.files;
+  try {
+    const { enrollment_no, program, program_code, remark } = req.fields;
+    const { project_file } = req.files;
 
-        console.log(req.files);
+    console.log(req.files);
 
+    if (!program || !enrollment_no || !project_file || !program_code) {
+      res.status(400).send({
+        success: false,
+        message: "All Fields are required",
+      });
+    } else if (!project_file.name?.includes(".pdf")) {
+      return res.status(400).send({
+        success: false,
+        message: "Only PDF File is allowed",
+      });
+    } else if (project_file.size > 2097152) {
+      return res.status(400).send({
+        success: false,
+        message: "Max File Size allowed is 2MB",
+      });
+    } else {
+      const user = dummyUsers.find((item) => item.enrollment === enrollment_no);
 
-        if (!program || !enrollment_no || !project_file || !program_code) {
-            res.status(400).send({
-                success: false,
-                message: "All Fields are required"
-            })
-        } else if (!project_file.name?.includes('.pdf')) {
-            return res.status(400).send({
-                success: false,
-                message: "Only PDF File is allowed"
-            })
-        } else if (project_file.size > 2097152) {
-            return res.status(400).send({
-                success: false,
-                message: "Max File Size allowed is 2MB"
-            })
-        } else {
+      if (!user) {
+        return res.status(401).send({
+          success: false,
+          message: "No Student With The Enrollment Found",
+        });
+      }
 
-            const user = dummyUsers.find((item) => item.enrollment === enrollment_no);
+      const submissioId =
+        `${enrollment_no}${program}${program_code}`.replaceAll("-", "");
 
-            if (!user) {
-                return res.status(401).send({
-                    success: false,
-                    message: "No Student With The Enrollment Found"
-                })
-            }
+      const results = await cloudinary.uploader.upload(project_file.path, {
+        public_id: `${enrollment_no}_${Math.floor(
+          Math.random() * 9000 + 1000
+        )}_file`,
+        resource_type: "auto",
+      });
 
-            const dateNow = Date.now();
+      if (results) {
+        const assignment = new assignment({
+          program: program,
+          program_code: program_code,
+          enrollment_no: enrollment_no,
+          project_file: results,
+          remark: remark ? remark : "",
+          submission_id: submissioId,
+        });
 
-            const submissioId = `${enrollment_no}${program}${program_code}`.replaceAll('-', "");
+        await assignment.save();
 
-            res.status(200).send({
-                success: true,
-                message: "Project Submitted Successfully",
-                data: {
-                    program,
-                    program_code,
-                    submissioId,
-                    project_file,
-                    dateNow
-                }
-            })
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({
-            success: false,
-            message: "Something went wrong"
-        })
+        res.status(200).send({
+          success: true,
+          message: "Project Submitted Successfully",
+          data: {
+            assignment,
+          },
+        });
+      }
     }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Something went wrong",
+    });
+  }
 }
---
+++ models/Assignment.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const AssignmentSchema = new mongoose.Schema(
+  {
+    program: {
+      type: String,
+      required: true,
+    },
+    program_code: {
+      type: String,
+      required: true,
+    },
+    enrollment_no: {
+      type: String,
+      required: true,
+    },
+    remark: {
+      type: String,
+      required: false,
+    },
+    project_file: {
+      type: {},
+      required: true,
+    },
+    submission_id: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Assignment", AssignmentSchema);
