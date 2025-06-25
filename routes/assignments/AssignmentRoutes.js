2582b232aa89381344220147ecb0d2abb02fc095
Arun <arunonwork3249@gmail.com>
2025-06-25 16:19:53 +0530

    Minor Fix For Assignment Route

--- routes/assignments/AssignmentRoutes.js
+++ routes/assignments/AssignmentRoutes.js
@@ -1,12 +1,15 @@
-import express from 'express';
-import submit from '../../controllers/assignments/submit.js';
-import ExpressFormidable from 'express-formidable';
-
+import express from "express";
+import submit from "../../controllers/assignments/submit.js";
+import ExpressFormidable from "express-formidable";
 
 const router = express.Router();
+router.use(ExpressFormidable());
+router.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-router.post('/submit', ExpressFormidable(), submit);
-
-
+router.post("/submit", ExpressFormidable(), submit);
 
 export default router;
