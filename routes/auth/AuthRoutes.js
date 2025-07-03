8e03047d7b0e4513bf323866a239f8c0df4c3859
Arun <arunonwork3249@gmail.com>
2025-07-03 16:01:33 +0530

    Minor Fix In Evaluators

--- routes/auth/AuthRoutes.js
+++ routes/auth/AuthRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import register from "../../controllers/auth/register.js";
 import signIn from "../../controllers/auth/signIn.js";
 import adminLogin from "../../controllers/auth/admin_login.js";
+import UserModel from "../../models/UserModel.js";
 
 const router = express.Router();
 
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import AssignmentRoutes from "./routes/assignments/AssignmentRoutes.js";
 import connectToDB from "./DB/database.js";
 import dotenv from "dotenv";
 import NotificationRoutes from "./routes/notifications/NotificationRoutes.js";
+import UserModel from "./models/UserModel.js";
 
 const app = express();
 const PORT = 4050;
@@ -26,6 +27,22 @@ app.use(express.json());
 app.use("/api/v1/auth", AuthRoutes);
 app.use("/api/v1/assignment", AssignmentRoutes);
 app.use("/api/v1/notifications", NotificationRoutes);
+
+app.use("/api/v1/evaluators", async (req, res) => {
+  try {
+    const evaluators = await UserModel.find({ role: "evaluator" });
+    console.log(evaluators)
+    if (evaluators?.length === 0) {
+      res.status(200).send({ success: false, message: "No Evaluators Found" });
+    } else {
+      res
+        .status(200)
+        .send({ success: true, message: "Evaluators Found", evaluators });
+    }
+  } catch (err) {
+    res.status(500).send({ error: "Assignment failed" });
+  }
+});
 
 app.get("/keep-alive", async (req, res) => {
   res.status(200).send({ success: true, message: "Server Is Alive" });
